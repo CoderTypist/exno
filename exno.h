97a20fbfefcb62939aca025388f34c220be6128e
Christian Bargraser <39784989+CoderTypist@users.noreply.github.com>
2022-08-01 21:00:01 -0500

    Create exno.h

---
+++ exno.h
@@ -0,0 +1,15 @@
+#ifndef _EXNO_H_
+#define _EXNO_H_
+
+
+#define MAX_ERRNO_NAME_LEN 19
+
+
+char* errname(int err);
+void errnout();
+void exitMsg(int status, void (*fnEnd)(), char* fmt, ...);
+void exno(int exitStatus, void (*fnEnd)(), char* msg);
+void exnoMsg(int exitStatus, void (*fnEnd)(), char* fmt, ...);
+
+
+#endif // _EXNO_H_

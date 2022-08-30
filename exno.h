8572a7a7f6dd02354c42f16da47d4e1a73283e8b
Christian Bargraser <39784989+CoderTypist@users.noreply.github.com>
2022-08-29 23:20:58 -0500

    Updated errname() function prototype.

--- exno.h
+++ exno.h
@@ -2,12 +2,9 @@
 #define _EXNO_H_
 
 
-#define MAX_ERRNO_NAME_LEN 19
-
-
-char* errname(int err);
+const char* const errname(int err);
 void errnout();
-void exitMsg(int exitStatus, void (*fnEnd)(), char* fmt, ...);
+void exitMsg(int status, void (*fnEnd)(), char* fmt, ...);
 void exno(int exitStatus, void (*fnEnd)(), char* msg);
 void exnoMsg(int exitStatus, void (*fnEnd)(), char* fmt, ...);
 
